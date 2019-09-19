336503f9b11d40f965f15788de16b47956dfe01d
Jordan Winslow <52429486+JordanWinslow@users.noreply.github.com>
2019-09-19 00:27:56 -0700

    Centered Bokeh Text
    Centered Bokeh Text

--- src/BokehText.js
+++ src/BokehText.js
@@ -4,6 +4,8 @@ import styled from "styled-components"
 const Animate = styled.div`
   font-size: 50px;
   color: white;
+  text-align: center;
+  line-height: 100vh;
 `
 
 const BokehText = props => {
